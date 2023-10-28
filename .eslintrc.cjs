933e6a75ed03b05c10f91c79952c8619a4acf2f9
Martin Emanuel <martinemanuel93@gmail.com>
2023-10-28 17:28:49 -0300

    basic setup and storybook

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/** @type import('eslint').Config */
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'storybook'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'semi': ['error', ]
+  },
+}
