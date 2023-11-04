b2a1046f0c4d463f09194eff7bda043cbc581887
Martin Emanuel <martinemanuel93@gmail.com>
2023-11-03 21:19:24 -0300

    add react hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,64 @@
 import { ButtonMacro } from '@ui';
-import { FormEvent, useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 
-// type Inputs = {
-// 	example: string;
-// 	exampleRequired: string;
-// };
+type MyInputs = {
+	email: string;
+	passwordRequired: string;
+};
 
 function App() {
-	// const {
-	// 	register,
-	// 	handleSubmit,
-	// 	watch,
-	// 	formState: { errors },
-	// } = useForm<Inputs>();
-	// const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<MyInputs>();
 
-	// console.log(watch('example'));
+	const onSubmit: SubmitHandler<MyInputs> = (data) => console.log(data);
 
-	const [email, setEmail] = useState('');
-	const [isSubmitting, setIsSubmitting] = useState(false);
+	console.log(watch('email'));
 
-	async function onSubmitFunction(e: FormEvent<HTMLFormElement>) {
-		e.preventDefault();
-		setIsSubmitting(true);
-		await new Promise((resolve) => setTimeout(resolve, 1500));
-		setIsSubmitting(false);
-	}
 	return (
-		<>
+		<div className="flex min-h-screen flex-col flex-wrap content-center justify-center">
 			<form
-				onSubmit={onSubmitFunction}
-				className="flex flex-col justify-evenly gap-2 rounded-sm border p-8"
+				onSubmit={handleSubmit(onSubmit)}
+				className="flex w-full max-w-sm flex-col justify-evenly gap-8 rounded-sm p-8 shadow-lg"
 			>
 				<input
+					{...register('email')}
 					type="text"
 					maxLength={20}
 					placeholder="Ingrese su nombre..."
 					className="mt-1 block w-full rounded-md border-gray-300  text-macro-blue shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
 				/>
 				<input
-					onChange={(e) => setEmail(e.target.value)}
 					type="email"
 					maxLength={20}
 					minLength={4}
-					value={email}
 					placeholder="Ingrese su email..."
 					className="mt-1 block w-full rounded-md border-gray-300  text-macro-blue shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
 				/>
 				<input
+					{...register('passwordRequired', {
+						required: { value: true, message: '*This field is required' },
+						minLength: 4,
+						maxLength: {
+							value: 20,
+							message: 'Password must to have at least 20 characters',
+						},
+					})}
 					type="password"
 					placeholder="Contraseña..."
-					required
-					maxLength={20}
 					className="mt-1 block w-full rounded-md border-gray-300  text-macro-blue shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 "
 				/>
-				<ButtonMacro type="submit" disabled={isSubmitting}>
-					{' '}
-					Submit{' '}
-				</ButtonMacro>
+				{errors.passwordRequired && (
+					<span className="italic text-red-500">
+						{errors.passwordRequired.message}
+					</span>
+				)}
+				<ButtonMacro type="submit"> Submit </ButtonMacro>
 			</form>
-		</>
+		</div>
 	);
 }
 
